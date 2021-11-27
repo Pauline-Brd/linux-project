b6e40880968ef773988d6363071bec26639c6623
Pauline-Brd <57439982+Pauline-Brd@users.noreply.github.com>
2021-11-27 21:03:21 +0100

    Create initAll.sh

---
+++ initAll.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+gpio4=4
+gpio17=17
+gpio22=22
+gpio10=10
+gpio9=9
+gpio11=11
+buzzer=7
+key=8
+construction=5
+#Ce script exporte toutes les broches GPIO et les définit en sortie/entrée
+for gpiono in $gpio4 $gpio17 $gpio22 $gpio10 $gpio9 $gpio11 $buzzer $key $construction
+do
+    if [ ! -d /sys/class/gpio/gpio$gpiono ]
+    then
+        echo $gpiono > /sys/class/gpio/export
+        sleep 1
+    fi
+done
+
+for gpiono in $gpio4 $gpio17 $gpio22 $gpio10 $gpio9 $gpio11 $buzzer $construction
+do
+    echo out > /sys/class/gpio/gpio$gpiono/direction
+done
+
+echo in > /sys/class/gpio/gpio$key/direction
