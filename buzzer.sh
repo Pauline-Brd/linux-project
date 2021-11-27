13e589da245e93034d0106906485f9f8ae2cc1bb
Pauline-Brd <57439982+Pauline-Brd@users.noreply.github.com>
2021-11-27 21:04:49 +0100

    Create buzzer.sh

---
+++ buzzer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ ! -d /sys/class/gpio/gpio7 ]
+then
+    echo 7 > /sys/class/gpio/export
+fi
+echo out > /sys/class/gpio/gpio7/direction
+i=0
+while [ $i != 32 ]
+do
+    sh -c "echo 0 > /sys/class/gpio/gpio7/value"
+    sh -c "echo 1 > /sys/class/gpio/gpio7/value"
+    sh -c "echo 0 > /sys/class/gpio/gpio7/value"
+    if [ $i -lt 10 ]
+    then
+        sleep 0.5
+        elif [ $i -lt 20 ]
+        then
+            sleep 0.3
+        else sleep 0.1
+    fi
+
+    let "i+=1" #i++
+done
